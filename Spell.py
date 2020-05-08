67b7f00481ce9d07e12f5e570f7277c62d37783a
Morihaus <villam1995@gmail.com>
2020-05-08 18:49:17 +0200

    Update Spell.py

--- Spell.py
+++ Spell.py
@@ -11,27 +11,22 @@ WORDS = Counter(words(open('big.txt').read()))
 
 
 def P(word, N=sum(WORDS.values())):
-    "Probability of `word`."
     return WORDS[word] / N
 
 
 def correction(word):
-    "Most probable spelling correction for word."
     return max(candidates(word), key=P)
 
 
 def candidates(word):
-    "Generate possible spelling corrections for word."
     return (known([word]) or known(edits1(word)) or known(edits2(word)) or [word])
 
 
 def known(words):
-    "The subset of `words` that appear in the dictionary of WORDS."
     return set(w for w in words if w in WORDS)
 
 
 def edits1(word):
-    "All edits that are one edit away from `word`."
     letters = 'abcdefghijklmnopqrstuvwxyz'
     splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
     deletes = [L + R[1:] for L, R in splits if R]
@@ -42,7 +37,6 @@ def edits1(word):
 
 
 def edits2(word):
-    "All edits that are two edits away from `word`."
     return (e2 for e1 in edits1(word) for e2 in edits1(e1))
 
 
